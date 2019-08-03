075f061295dc5989c2874cea47b609a025924cc5
karabanb <bkarabands@gmail.com>
2019-08-03 10:21:11 +0200

    fibo memoization

--- fibo.py
+++ fibo.py
@@ -1,10 +1,19 @@
 
+cache = {}
+
+
 def fibonacci(n):
-    if n in (1,2):
+    if n in cache:
+        return cache[n]
+
+    if n in (1, 2):
+        cache[n] = 1
         return 1
-    return fibonacci(n - 2) + fibonacci(n - 1)
+
+    cache[n] = fibonacci(n - 2) + fibonacci(n - 1)
+    return cache[n]
 
 
-for i in range(1, 26):
+for i in range(1, 100):
     print(f'{i}: {fibonacci(i)}')
 
