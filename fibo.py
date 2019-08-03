373e080bcf8eb174fefacbc3f722fd3be8e35c8c
karabanb <bkarabands@gmail.com>
2019-08-03 10:29:43 +0200

    fibo with decorator

--- fibo.py
+++ fibo.py
@@ -17,3 +17,25 @@ def fibonacci(n):
 for i in range(1, 100):
     print(f'{i}: {fibonacci(i)}')
 
+
+def fib_decorator(decorated):
+    cache = {}
+
+    def wrapper(n):
+        try:
+            return cache[n]
+        except KeyError:
+            cache[n] = decorated(n)
+            return cache[n]
+    return wrapper
+
+
+@fib_decorator
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+for i in range(1, 100):
+    print(f'{i}: {fibonacci(i)}')
